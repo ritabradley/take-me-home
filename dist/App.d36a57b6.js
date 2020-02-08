145c01ab1da2fd36fa8a0bc1995588f615bf1203
Rita Bradley <contact@ritabradley.com>
2020-02-07 23:07:22 -0800

    Create custom hook

--- dist/App.d36a57b6.js
+++ dist/App.d36a57b6.js
@@ -33404,7 +33404,63 @@ if (undefined === "mock") {
 }
 
 module.exports.ANIMALS = require("./animals");
-},{"./impl":"../node_modules/@frontendmasters/pet/impl.js","./animals":"../node_modules/@frontendmasters/pet/animals.js"}],"SearchParams.js":[function(require,module,exports) {
+},{"./impl":"../node_modules/@frontendmasters/pet/impl.js","./animals":"../node_modules/@frontendmasters/pet/animals.js"}],"useDropdown.js":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireWildcard(require("react"));
+
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function () { return cache; }; return cache; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
+
+function _iterableToArrayLimit(arr, i) { if (!(Symbol.iterator in Object(arr) || Object.prototype.toString.call(arr) === "[object Arguments]")) { return; } var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+var useDropdown = function useDropdown(label, defaultState, options) {
+  var _useState = (0, _react.useState)(defaultState),
+      _useState2 = _slicedToArray(_useState, 2),
+      state = _useState2[0],
+      updateState = _useState2[1];
+
+  var id = "use-dropdown-".concat(label.replace(' ', '').toLowerCase());
+
+  var Dropdown = function Dropdown() {
+    return _react.default.createElement("label", {
+      htmlFor: id
+    }, label, _react.default.createElement("select", {
+      id: id,
+      value: state,
+      onChange: function onChange(e) {
+        return updateState(e.target.value);
+      },
+      onBlur: function onBlur(e) {
+        return updateState(e.target.value);
+      },
+      disabled: !options.length
+    }, _react.default.createElement("option", null), options.map(function (item) {
+      return _react.default.createElement("option", {
+        key: item,
+        value: item
+      }, item);
+    })));
+  };
+
+  return [state, Dropdown];
+};
+
+var _default = useDropdown;
+exports.default = _default;
+},{"react":"../node_modules/react/index.js"}],"SearchParams.js":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -33415,6 +33471,10 @@ exports.default = void 0;
 var _react = _interopRequireWildcard(require("react"));
 
 var _pet = require("@frontendmasters/pet");
+
+var _useDropdown5 = _interopRequireDefault(require("./useDropdown"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function () { return cache; }; return cache; }
 
@@ -33434,59 +33494,26 @@ var SearchParams = function SearchParams() {
       location = _useState2[0],
       updateLocation = _useState2[1];
 
-  var _useState3 = (0, _react.useState)(''),
+  var _useState3 = (0, _react.useState)([]),
       _useState4 = _slicedToArray(_useState3, 2),
-      animal = _useState4[0],
-      updateAnimal = _useState4[1];
+      breeds = _useState4[0],
+      updateBreeds = _useState4[1];
 
-  var _useState5 = (0, _react.useState)(''),
-      _useState6 = _slicedToArray(_useState5, 2),
-      breed = _useState6[0],
-      updateBreed = _useState6[1];
+  var _useDropdown = (0, _useDropdown5.default)('Animal', 'dog', _pet.ANIMALS),
+      _useDropdown2 = _slicedToArray(_useDropdown, 2),
+      animal = _useDropdown2[0],
+      AnimalDropdown = _useDropdown2[1];
 
-  var _useState7 = (0, _react.useState)([]),
-      _useState8 = _slicedToArray(_useState7, 2),
-      breeds = _useState8[0],
-      updateBreeds = _useState8[1];
+  var _useDropdown3 = (0, _useDropdown5.default)('Breed', '', breeds),
+      _useDropdown4 = _slicedToArray(_useDropdown3, 2),
+      breed = _useDropdown4[0],
+      BreedDropdown = _useDropdown4[1];
 
   return _react.default.createElement("div", {
     className: "search-params"
   }, _react.default.createElement("form", null, _react.default.createElement("label", {
     htmlFor: "location"
-  }, _react.default.createElement("label", {
-    htmlFor: "animal"
-  }, "Animal", _react.default.createElement("select", {
-    value: animal,
-    id: "animal",
-    onChange: function onChange(e) {
-      return updateAnimal(e.target.value);
-    },
-    onBlur: function onBlur(e) {
-      return updateAnimal(e.target.value);
-    }
-  }, _react.default.createElement("option", null), _pet.ANIMALS.map(function (animal) {
-    return _react.default.createElement("option", {
-      key: animal,
-      value: animal
-    }, animal);
-  }))), _react.default.createElement("label", {
-    htmlFor: "breed"
-  }, "Breed", _react.default.createElement("select", {
-    disabled: !breeds.length,
-    id: "breed",
-    value: breed,
-    onChange: function onChange(e) {
-      return updateBreed(e.target.value);
-    },
-    onBlur: function onBlur(e) {
-      return updateBreed(e.target.value);
-    }
-  }, _react.default.createElement("option", null), breeds.map(function (breed) {
-    return _react.default.createElement("option", {
-      key: breed,
-      value: breed
-    }, breed);
-  }))), "Location", _react.default.createElement("input", {
+  }, _react.default.createElement(AnimalDropdown, null), _react.default.createElement(BreedDropdown, null), "Location", _react.default.createElement("input", {
     id: "location",
     value: location,
     placeholder: "Location",
@@ -33498,7 +33525,7 @@ var SearchParams = function SearchParams() {
 
 var _default = SearchParams;
 exports.default = _default;
-},{"react":"../node_modules/react/index.js","@frontendmasters/pet":"../node_modules/@frontendmasters/pet/index.js"}],"App.js":[function(require,module,exports) {
+},{"react":"../node_modules/react/index.js","@frontendmasters/pet":"../node_modules/@frontendmasters/pet/index.js","./useDropdown":"useDropdown.js"}],"App.js":[function(require,module,exports) {
 "use strict";
 
 var _react = _interopRequireDefault(require("react"));
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,48 +1,18 @@
 import React, { useState } from 'react'
 import { ANIMALS } from '@frontendmasters/pet'
+import useDropdown from './useDropdown'
 
 const SearchParams = () => {
     const [location, updateLocation] = useState('Las Vegas, NV')
-    const [animal, updateAnimal] = useState('')
-    const [breed, updateBreed] = useState('')
     const [breeds, updateBreeds] = useState([])
+    const [animal, AnimalDropdown] = useDropdown('Animal', 'dog', ANIMALS)
+    const [breed, BreedDropdown] = useDropdown('Breed', '', breeds)
     return (
         <div className='search-params'>
             <form>
                 <label htmlFor='location'>
-                    <label htmlFor='animal'>
-                        Animal
-                        <select
-                            value={animal}
-                            id='animal'
-                            onChange={e => updateAnimal(e.target.value)}
-                            onBlur={e => updateAnimal(e.target.value)}
-                        >
-                            <option />
-                            {ANIMALS.map(animal => (
-                                <option key={animal} value={animal}>
-                                    {animal}
-                                </option>
-                            ))}
-                        </select>
-                    </label>
-                    <label htmlFor='breed'>
-                        Breed
-                        <select
-                            disabled={!breeds.length}
-                            id='breed'
-                            value={breed}
-                            onChange={e => updateBreed(e.target.value)}
-                            onBlur={e => updateBreed(e.target.value)}
-                        >
-                            <option />
-                            {breeds.map(breed => (
-                                <option key={breed} value={breed}>
-                                    {breed}
-                                </option>
-                            ))}
-                        </select>
-                    </label>
+                    <AnimalDropdown />
+                    <BreedDropdown />
                     Location
                     <input
                         id='location'
