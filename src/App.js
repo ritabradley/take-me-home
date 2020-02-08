bf6b37402df72a1192d82970f42a7c0644522b72
Rita Bradley <contact@ritabradley.com>
2020-02-07 21:04:35 -0800

    Setup Parcel, Prettier and ESLint

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { render } from 'react-dom'
+import Pet from './Pet'
+
+const App = () => {
+    return (
+        <div>
+            <h1>Adopt Me</h1>
+            <Pet name='Luna' animal='Dog' breed='Havanese' />
+            <Pet name='Pepper' animal='Bird' breed='Cockatiel' />
+            <Pet name='Doink' animal='Cat' breed='Mix' />
+        </div>
+    )
+}
+render(React.createElement(App), document.getElementById('root'))
